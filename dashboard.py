9f885920fc202b48bb8d9a30c6eb074356bcc0d1
gillianline <gillian.line@yahoo.com>
2026-05-11 19:42:40 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go  # <--- ADD THIS LINE
 
 # --- PAGE CONFIG ---
 st.set_page_config(page_title="Softball Performance Hub", layout="wide")
